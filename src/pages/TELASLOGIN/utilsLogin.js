92e887c2c0df6b0055e392321981dfadf71989cf
PAULO HENRIQUE GUIDIO DIAS <paulo.hgdias@senacsp.edu.br>
2025-11-26 21:40:48 -0300

    teste

--- src/pages/TELASLOGIN/utilsLogin.js
+++ src/pages/TELASLOGIN/utilsLogin.js
@@ -30,49 +30,32 @@ export async function verificarCredenciais(url, credenciais, form) {
       body: JSON.stringify(credenciais),
     });
 
-    // 5. TRATAMENTO DA RESPOSTA
     if (response.ok) {
-      // 200 OK: Credenciais encontradas e válidas.
+
+      const data = await response.json();
+      
+
+      localStorage.setItem("usuarioLogado", JSON.stringify(data));
+
       exibirAlerta(form, "success", "Login bem-sucedido! Redirecionando...");
 
-      // Redirecionamento para a Landing Page (LP.html)
-      // Note: O caminho é '../TELASLANDINGPAGE/LP.html' conforme seu HTML
       setTimeout(
         () => (window.location.href = "../TELASLANDINGPAGE/LP.html"),
         1500
       );
-    } else if (
-      response.status === 401 ||
-      response.status === 400 ||
-      response.status === 404
-    ) {
-      let erroMensagem =
-        "E-mail ou senha incorretos ou inexistentes. Tente novamente.";
-
+    } else {
+    
+      let erroMensagem = "E-mail ou senha incorretos.";
       try {
         const erroJson = await response.json();
-        // Assumindo que o Spring Boot pode retornar uma mensagem de erro JSON
-        erroMensagem = erroJson.mensagem || erroJson.error || erroMensagem;
+        erroMensagem = erroJson.message || erroJson.error || erroMensagem;
       } catch (e) {
-        // Se falhar (Backend enviou texto simples), lemos o texto.
         const erroTexto = await response.text();
-        // Se o texto não for vazio, usamos ele, senão o fallback.
-        if (erroTexto.trim() !== "") {
-          erroMensagem = erroTexto;
-        }
+        if (erroTexto) erroMensagem = erroTexto;
       }
-
       exibirAlerta(form, "error", erroMensagem);
-    } else {
-      // Outros erros do servidor (ex: 500 Internal Server Error)
-      exibirAlerta(
-        form,
-        "error",
-        `Erro no servidor (${response.status}): Tente novamente mais tarde.`
-      );
     }
   } catch (error) {
-    // Erros de rede (Network Failure)
     exibirAlerta(form, "error", "Falha na comunicação: Servidor inacessível.");
     console.error("Erro de conexão:", error);
   }
